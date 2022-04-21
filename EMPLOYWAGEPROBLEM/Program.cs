e7b9d2897ecb1468af3914f3a67a66ca0432f116
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-04-21 17:51:37 +0530

    Checking attendance

--- EMPLOYWAGEPROBLEM/Program.cs
+++ EMPLOYWAGEPROBLEM/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("welcome to employee wage problem!");
+Console.WriteLine("Welcome to Employ Wage Computation Program!");
+Random attendanceCheck = new();
+int isPresent = attendanceCheck.Next(0, 2);
+Console.WriteLine(isPresent);
+if (isPresent == 1)
+    Console.WriteLine("Employee is present!");
+else
+    Console.WriteLine("Employee is Absent!");
