6d58a8a4b654925744266d8a27bd2b3a191bf596
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-04-21 17:59:25 +0530

    adding a part time employee

--- EMPLOYWAGEPROBLEM/Program.cs
+++ EMPLOYWAGEPROBLEM/Program.cs
@@ -3,17 +3,23 @@ Console.WriteLine("Welcome to Employ Wage Computation Program!");
 int WagePerHour = 20;
 int WorkingHours;
 Random attendanceCheck = new();
-int isPresent = attendanceCheck.Next(0, 2);
+int isPresent = attendanceCheck.Next(0, 3);
 Console.WriteLine(isPresent);
 if (isPresent == 1)
 {
-    Console.WriteLine("Employee is present!");
+    Console.WriteLine("Employee is present and working Full Time!");
     WorkingHours = 8;
+}
+else if (isPresent == 2)
+{
+    Console.WriteLine("Employee is working for part Time!");
+    WorkingHours = 4;
 }
 else
 {
     Console.WriteLine("Employee is Absent!");
     WorkingHours = 0;
+    //if isPresent is 2 then we consider it as part time and working hours are 4
 }
 int EmployeeWage = WagePerHour * WorkingHours;
-Console.WriteLine("The Employee wage is : " + EmployeeWage);
+Console.WriteLine("The Employee Daily Wage is : " + EmployeeWage);
